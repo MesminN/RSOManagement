0510056f7f8396af187faa4da02d23f8dcdddce5
MesminN <70533175+MesminN@users.noreply.github.com>
2021-01-14 20:21:26 -0600

    update code

--- rso-management-business/src/main/java/com/rso/multilayered/model/RSO.java
+++ rso-management-business/src/main/java/com/rso/multilayered/model/RSO.java
@@ -1,20 +1,43 @@
 package com.rso.multilayered.model;
 
-import com.rso.multilayered.utils.Advisor;
-import com.rso.multilayered.utils.President;
 import com.rso.multilayered.utils.RsoStatus;
+import com.rso.multilayered.utils.StatusAgreement;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class RSO {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotNull("Name can't be null")
     private String name;
     private String address;
     private RsoStatus status;
     private String notes;
-    private President president;
-    private Advisor advisor;
+
+    // President
+    @NotNull("President's name can't be null")
+    private String presidentName;
+    @NotNull("President's email address can't be null")
+    private String presidentAddress;
+
+    //Advisor
+    @NotNull("Advisor's name can't be null")
+    private String advisorName;
+    @NotNull("Advisor's email address can't be null")
+    private String advisorAddress;
+    @NotNull("Advisor's title can't be null")
+    private String advisorTitle;
+    private String advisorPhoneNumber;
+    private StatusAgreement advisorStatusAgreement;
+
+    // booleans
     private Boolean isRenewed;
     private Boolean osRosterSubmitted;
     private Boolean isRoomReserved;
@@ -32,11 +55,12 @@ public class RSO {
         this.id = id;
     }
 
+    @NotNull
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(@NotNull String name) {
         this.name = name;
     }
 
@@ -62,22 +86,6 @@ public class RSO {
 
     public void setNotes(String notes) {
         this.notes = notes;
-    }
-
-    public President getPresident() {
-        return president;
-    }
-
-    public void setPresident(President president) {
-        this.president = president;
-    }
-
-    public Advisor getAdvisor() {
-        return advisor;
-    }
-
-    public void setAdvisor(Advisor advisor) {
-        this.advisor = advisor;
     }
 
     public Boolean isRenewed() {
@@ -120,26 +128,64 @@ public class RSO {
         isAddedToAdvocate = addedToAdvocate;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RSO)) return false;
-        RSO rso = (RSO) o;
-        return getId().equals(rso.getId()) &&
-                getName().equals(rso.getName()) &&
-                Objects.equals(getAddress(), rso.getAddress()) &&
-                getStatus() == rso.getStatus() &&
-                Objects.equals(getPresident(), rso.getPresident()) &&
-                Objects.equals(getAdvisor(), rso.getAdvisor()) &&
-                Objects.equals(isRenewed, rso.isRenewed) &&
-                Objects.equals(osRosterSubmitted, rso.osRosterSubmitted) &&
-                Objects.equals(isRoomReserved, rso.isRoomReserved) &&
-                Objects.equals(isOnWebsite, rso.isOnWebsite) &&
-                Objects.equals(isAddedToAdvocate, rso.isAddedToAdvocate);
+    @NotNull
+    public String getPresidentName() {
+        return presidentName;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getAddress(), getStatus(), getPresident(), getAdvisor());
+    public void setPresidentName(@NotNull String presidentName) {
+        this.presidentName = presidentName;
+    }
+
+    @NotNull
+    public String getPresidentAddress() {
+        return presidentAddress;
+    }
+
+    public void setPresidentAddress(@NotNull String presidentAddress) {
+        this.presidentAddress = presidentAddress;
+    }
+
+    @NotNull
+    public String getAdvisorName() {
+        return advisorName;
+    }
+
+    public void setAdvisorName(@NotNull String advisorName) {
+        this.advisorName = advisorName;
+    }
+
+    @NotNull
+    public String getAdvisorAddress() {
+        return advisorAddress;
+    }
+
+    public void setAdvisorAddress(@NotNull String advisorAddress) {
+        this.advisorAddress = advisorAddress;
+    }
+
+    @NotNull
+    public String getAdvisorTitle() {
+        return advisorTitle;
+    }
+
+    public void setAdvisorTitle(@NotNull String advisorTitle) {
+        this.advisorTitle = advisorTitle;
+    }
+
+    public String getAdvisorPhoneNumber() {
+        return advisorPhoneNumber;
+    }
+
+    public void setAdvisorPhoneNumber(String advisorPhoneNumber) {
+        this.advisorPhoneNumber = advisorPhoneNumber;
+    }
+
+    public StatusAgreement getAdvisorStatusAgreement() {
+        return advisorStatusAgreement;
+    }
+
+    public void setAdvisorStatusAgreement(StatusAgreement advisorStatusAgreement) {
+        this.advisorStatusAgreement = advisorStatusAgreement;
     }
 }
