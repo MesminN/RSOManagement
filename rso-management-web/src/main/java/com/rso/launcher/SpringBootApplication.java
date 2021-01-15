6605cd899190328a62e05d97413b36e5cbe87eb6
MesminN <70533175+MesminN@users.noreply.github.com>
2021-01-15 10:31:24 -0600

    import EntityScan

--- rso-management-web/src/main/java/com/rso/launcher/SpringBootApplication.java
+++ rso-management-web/src/main/java/com/rso/launcher/SpringBootApplication.java
@@ -3,6 +3,7 @@ package com.rso.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,8 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan({"com.rso"})
-/*@EnableJpaRepositories({"com.rso"})
-@EntityScan({"com.rso"})*/
+/*@EnableJpaRepositories({"com.rso"})*/
+@EntityScan(basePackages = {"com.rso"})
 public class SpringBootApplication extends SpringBootServletInitializer {
 
     @Override
