897ef1d544ff9f4a75be5c4ba75414c765b31f26
MesminN <70533175+MesminN@users.noreply.github.com>
2021-01-14 20:51:35 -0600

    update code

--- rso-management-business/src/main/java/com/rso/multilayered/controller/RSOController.java
+++ rso-management-business/src/main/java/com/rso/multilayered/controller/RSOController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rsos")
+@RequestMapping("/rso")
 public class RSOController {
 
     @Autowired
